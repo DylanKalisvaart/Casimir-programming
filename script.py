ab3bd8bdcce7e86ba1baa42d596dd592848fbcd6
DylanKalisvaart <d.kalisvaart@tudelft.nl>
2021-01-25 13:44:56 +0100

    Import test.py in script.py, compute circumference and area of circle

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
+import test
+
 print('some print statement')
+
+r = 2
+circumference = test.circumference_circle(r)
+area = test.area_circle(r)
+
+print('Circumference: ', circumference)
+print('Area: ', area)
