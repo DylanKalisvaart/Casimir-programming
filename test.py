b2e26981085e9ef19c610cb36fd824bb5874c166
DylanKalisvaart <d.kalisvaart@tudelft.nl>
2021-01-25 14:45:17 +0100

    Docstring to area function

--- test.py
+++ test.py
@@ -4,6 +4,15 @@ def circumference_circle(r):
     return 2*np.pi*r
 
 def area_circle(r):
+    '''
+    area = area_circle(r) computes the area of a circle, given its radius r
+    
+    Inputs:
+       :param float r: radius of circle
+       
+    Outputs:
+       :param float area: area of circle
+    '''
     return np.pi*r**2
 
 print('hello world')
